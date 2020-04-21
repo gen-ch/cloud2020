e3cf0fe6266525ff54866aa726c7eeda18a92afd
gen <1757244573@qq.com>
2020-04-21 21:23:09 +0800

    commit

---
+++ cloud-provider-payment8001/src/main/java/com/gen/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,25 @@
+package com.gen.springcloud.service.impl;
+
+import com.gen.springcloud.dao.PaymentDao;
+import com.gen.springcloud.entities.Payment;
+import com.gen.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Resource
+    private PaymentDao paymentDao;
+
+    @Override
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+        return paymentDao.getPaymentById(id);
+    }
+}
