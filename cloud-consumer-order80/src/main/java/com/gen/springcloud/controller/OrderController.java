a4509fd571d278ec2ff96e056f9f763de889fffe
wade_chen <wade_chen@ehsy.com>
2020-06-29 09:53:49 +0800

    no message

--- cloud-consumer-order80/src/main/java/com/gen/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/gen/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.gen.springcloud.controller;
 import com.gen.springcloud.entities.CommonResult;
 import com.gen.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +15,9 @@ import javax.annotation.Resource;
 @Slf4j
 public class OrderController {
 
-//    public static final String PAYMENT_URL = "http://localhost:8001";
-    public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
+    private static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
-    @Resource
+    @Autowired
     private RestTemplate restTemplate;
 
     @GetMapping("/consumer/payment/create")
