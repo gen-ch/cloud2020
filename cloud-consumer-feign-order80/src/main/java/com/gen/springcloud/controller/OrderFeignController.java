4eab3e7b4683eb9ca5d21374848e6ce65ee20cd0
wade_chen <wade_chen@ehsy.com>
2020-07-08 19:03:24 +0800

    no message

---
+++ cloud-consumer-feign-order80/src/main/java/com/gen/springcloud/controller/OrderFeignController.java
@@ -0,0 +1,20 @@
+package com.gen.springcloud.controller;
+
+import com.gen.springcloud.entities.CommonResult;
+import com.gen.springcloud.entities.Payment;
+import com.gen.springcloud.service.PaymentFeignService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderFeignController {
+    @Autowired
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping(value = "/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
+        return paymentFeignService.getPaymentById(id);
+    }
+}
