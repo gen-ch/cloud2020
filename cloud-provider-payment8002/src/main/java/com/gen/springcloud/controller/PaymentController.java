237e549d4c2c1425bf4a3e5a41c1098c2185cabb
wade_chen <wade_chen@ehsy.com>
2020-07-09 18:53:58 +0800

    no message

--- cloud-provider-payment8002/src/main/java/com/gen/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/gen/springcloud/controller/PaymentController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @Slf4j
@@ -44,4 +45,16 @@ public class PaymentController {
     public String getPaymentLB() {
         return serverPort;
     }
+
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeout() {
+        // 业务逻辑处理正确，但是需要耗费3秒钟
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return serverPort;
+    }
+
 }
