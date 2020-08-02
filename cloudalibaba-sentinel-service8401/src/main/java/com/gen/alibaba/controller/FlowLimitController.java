7e62e5d486b44861abca267fa6477053e9587b0c
gen <1757244573@qq.com>
2020-08-02 22:20:49 +0800

    no message

--- cloudalibaba-sentinel-service8401/src/main/java/com/gen/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/gen/alibaba/controller/FlowLimitController.java
@@ -3,6 +3,8 @@ package com.gen.alibaba.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class FlowLimitController {
 
@@ -13,7 +15,19 @@ public class FlowLimitController {
 
     @GetMapping("/testB")
     public String testB() {
-//        System.out.println((Thread.currentThread().getName() + "\t" + "...testB"));
+        System.out.println((Thread.currentThread().getName() + "\t" + "...testB"));
         return "------testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("TestD 测试RT");
+//        int age = 10 / 0;
+        return "------testD";
     }
 }
