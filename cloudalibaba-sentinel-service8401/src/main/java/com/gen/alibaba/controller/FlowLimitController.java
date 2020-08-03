af376cc8df3370750b53c7b59506fd4ed8be1545
wade_chen <wade_chen@ehsy.com>
2020-08-03 16:10:20 +0800

    no message

--- cloudalibaba-sentinel-service8401/src/main/java/com/gen/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/gen/alibaba/controller/FlowLimitController.java
@@ -1,6 +1,9 @@
 package com.gen.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -29,5 +32,18 @@ public class FlowLimitController {
         System.out.println("TestD 测试RT");
 //        int age = 10 / 0;
         return "------testD";
+    }
+
+    @GetMapping("/testHotKey")
+// value值可以随意起，但为唯一标识，blockHandler是处理方法
+    @SentinelResource(value = "testHotKey", blockHandler = "deal_testHotKey")
+    public String testHostKey(@RequestParam(value = "p1", required = false) String p1,
+                              @RequestParam(value = "p2", required = false) String p2) {
+        System.out.println(p1);
+        return "testHostKey";
+    }
+
+    public String deal_testHotKey(String p1, String p2, BlockException exception) {
+        return "deal_testHotKey";
     }
 }
